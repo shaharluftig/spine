0211b6acf61356bcea87d45760a88cb5e2e4f64f
Luftig <shaharluftig@gmail.com>
2023-02-18 23:06:16 +0200

    chore(add some typing)

--- core/src/executors/IExecutor.py
+++ core/src/executors/IExecutor.py
@@ -1,7 +1,6 @@
-from core.src.common.context.IContext import IContext
 from core.src.workflows.Workflow import Workflow
 
 
 class IExecutor:
-    async def execute(self, workflow: Workflow, cardo_context: IContext):
+    async def execute(self, workflow: Workflow):
         raise NotImplementedError()
--- core/src/executors/implementations/workflow_executor.py
+++ core/src/executors/implementations/workflow_executor.py
@@ -15,26 +15,16 @@ class WorkflowExecutor(IExecutor):
         result = cardo_logger.log_step(step.process)
         return await result(self.ctx, *dependencies)
 
-    async def __get_dependency_results(self, dependencies, step: IStep, steps_results, workflow: Workflow):
-        dependencies_results = []
-        for dependency in dependencies:
-            dependency_result = await self.__execute_step_by_dependencies(workflow, steps_results, dependency)
-            if isinstance(dependency_result, list) or isinstance(dependency_result, tuple):
-                if len(workflow.get_after(dependency)) > 1:
-                    dependency_result = dependency_result[workflow.get_after(dependency).index(step)]
-                    dependencies_results.append(dependency_result)
-                else:
-                    dependencies_results.extend(dependency_result)
-            else:
-                dependencies_results.append(dependency_result)
-        return dependencies_results
+    async def __get_dependency_results(self, dependencies, steps_results: dict, workflow: Workflow):
+        return [await self.__execute_step_by_dependencies(workflow, steps_results, dependency) for
+                dependency in dependencies]
 
-    async def __execute_step_by_dependencies(self, workflow: Workflow, steps_results, step: IStep):
+    async def __execute_step_by_dependencies(self, workflow: Workflow, steps_results: dict, step: IStep):
         if step in steps_results:
             return steps_results[step]
         else:
             dependencies = workflow.get_before(step)
-            dependencies_results = await self.__get_dependency_results(dependencies, step, steps_results, workflow)
+            dependencies_results = await self.__get_dependency_results(dependencies, steps_results, workflow)
             steps_results[step] = await self.__execute_step(step, dependencies_results)
 
     async def __execute_all_steps(self, workflow: Workflow):
