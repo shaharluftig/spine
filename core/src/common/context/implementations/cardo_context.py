9802d3bc19141fdd0be56b1362575ba035b193a4
Luftig <shaharluftig@gmail.com>
2023-02-22 19:34:02 +0200

    fix(logger logic)

--- core/src/common/context/implementations/cardo_context.py
+++ core/src/common/context/implementations/cardo_context.py
@@ -3,17 +3,21 @@ import uuid
 from pyspark.sql import SparkSession
 
 from core.src.common.context.IContext import IContext
-from core.src.common.helpers.logging.cardo_logger import cardo_logger
+from core.src.common.helpers.logging.cardo_logger import CardoLogger
 from core.src.common.helpers.singleton import Singleton
 
 
 class CardoContext(IContext, metaclass=Singleton):
     def __init__(self, spark_session: SparkSession = None):
-        self.logger = cardo_logger.get_logger()
         self.run_id = str(uuid.uuid1()).lower()
+        self.__cardo_logger = CardoLogger(self.run_id)
+        self.logger = self.__cardo_logger.logger
         if spark_session:
             self.spark = spark_session
 
     def get_context(self):
-        self.logger.info(f"Starting cardo context with run_id: {self.run_id}")
+        self.logger.info(f"Starting Cardo context")
         return self
+
+    def get_cardo_logger(self):
+        return self.__cardo_logger
--- core/src/common/helpers/logging/cardo_logger.py
+++ core/src/common/helpers/logging/cardo_logger.py
@@ -5,13 +5,17 @@ from functools import wraps
 
 from core.src.common.helpers.singleton import Singleton
 
-logging.basicConfig(level=logging.INFO, handlers=[
-    logging.StreamHandler(sys.stdout)], format='%(asctime)s %(levelname)-6s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-
 
 class CardoLogger(metaclass=Singleton):
-    def __init__(self, name: str = "CardoLogger"):
+    def __init__(self, run_id: str, name: str = "CardoLogger"):
+        self.run_id = run_id
+        self.setup_logger()
         self.logger = logging.getLogger(name)
+
+    def setup_logger(self):
+        logging.basicConfig(level=logging.INFO, handlers=[
+            logging.StreamHandler(sys.stdout)], format=f'{self.run_id} | %(asctime)s %(levelname)s | %(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S')
 
     def log_step(self, func):
         @wraps(func)
@@ -24,9 +28,3 @@ class CardoLogger(metaclass=Singleton):
             return result
 
         return step_wrapper
-
-    def get_logger(self):
-        return self.logger
-
-
-cardo_logger = CardoLogger()
