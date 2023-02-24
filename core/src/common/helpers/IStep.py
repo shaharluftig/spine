660cdd89ae157b4cdff2b9f611c875444e548364
Luftig <shaharluftig@gmail.com>
2023-02-24 18:34:51 +0200

    fix(IStep type)

--- core/src/common/helpers/IStep.py
+++ core/src/common/helpers/IStep.py
@@ -1,10 +1,13 @@
+from typing import Union, Tuple
+
 from core.src.common.context.implementations.cardo_context import CardoContext
 from core.src.common.helpers.dataframe import DataFrame
 
 
 class IStep:
 
-    async def process(self, cardo_context: CardoContext, *df: DataFrame) -> DataFrame:
+    async def process(self, cardo_context: CardoContext, df: Union[DataFrame, Tuple[DataFrame], None]) \
+            -> Union[DataFrame, Tuple[DataFrame]]:
         raise NotImplementedError
 
     def __repr__(self):
