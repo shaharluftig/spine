21f3031f93000272bb6f8e241c27a5949e2aac74
Luftig <shaharluftig@gmail.com>
2023-02-24 20:55:39 +0200

    feat(SQLite reader)

--- examples/polars_exp/polars_exp.py
+++ examples/polars_exp/polars_exp.py
@@ -1,23 +1,26 @@
 import asyncio
+import os
 
 from core import DagWorkflow, CardoContext, execute
 from libs.src.steps.polars.io.console_writer import ConsoleWriter
 from libs.src.steps.polars.io.csv_reader import CsvReader
+from libs.src.steps.polars.io.sql_reader import SQLReader
 
 
 def __setup_steps():
+    db_path = os.path.dirname(os.path.realpath("../polars_exp/db/exp.db"))
     rent_reader = CsvReader("../polars_exp/resources/rent.csv", has_headers=True)
-    names_reader = CsvReader("../polars_exp/resources/names.csv", has_headers=True)
+    users_reader = SQLReader("select * FROM users", f"sqlite:{db_path}\\exp.db")
     console_output = ConsoleWriter()
-    return rent_reader, names_reader, console_output
+    return rent_reader, users_reader, console_output
 
 
 def workflow_factory():
     workflow = DagWorkflow()
-    rent_reader, names_reader, console_output = __setup_steps()
-    workflow.add_last(rent_reader, names_reader)
+    rent_reader, users_reader, console_output = __setup_steps()
+    workflow.add_last(rent_reader, users_reader)
     workflow.add_after([console_output], [rent_reader])
-    workflow.add_after([console_output], [names_reader])
+    workflow.add_after([console_output], [users_reader])
     return workflow
 
 
