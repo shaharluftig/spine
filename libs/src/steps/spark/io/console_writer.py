f2461947eb55e770fa1b0ec982b3b2ed65740dcd
Luftig <shaharluftig@gmail.com>
2023-02-24 21:02:55 +0200

    chore(docstrings)

--- libs/src/steps/spark/io/console_writer.py
+++ libs/src/steps/spark/io/console_writer.py
@@ -2,6 +2,8 @@ from core import IStep, DataFrame, CardoContext
 
 
 class ConsoleWriter(IStep):
+    """Prints dataframes to console"""
+
     async def process(self, cardo_context: CardoContext, *dfs: DataFrame) -> DataFrame:
         cardo_context.logger.info("Writing to console")
         for df in dfs:
