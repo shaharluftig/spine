cacbd3fa2c917a5ef1785d2cec7fbfef6943406d
Luftig <shaharluftig@gmail.com>
2023-03-06 22:38:46 +0200

    feat(polars sql writer)

--- libs/steps/polars/io/sql/sql_reader.py
+++ libs/steps/polars/io/sql/sql_reader.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 import polars as pl
 
 from core.common.context import GarnetPolarsContext
@@ -8,12 +10,14 @@ from core.common.helpers.steps import PolarsStep
 class SQLReader(PolarsStep):
     """A Generic SQL Database reader"""
 
-    def __init__(self, sql_query: str, connection_string: str, partition_on: str = None):
+    def __init__(self, sql_query: list[str] | str, connection_string: str, partition_on: str = None,
+                 engine: Literal["adbc", "connectorx"] = "connectorx"):
+        self.engine = engine
         self.sql_query = sql_query
         self.partition_on = partition_on
         self.connection_string = connection_string
 
     async def process(self, ctx: GarnetPolarsContext, df: PolarsDataFrame = None) -> PolarsDataFrame:
         ctx.logger.info(f"Querying: ({self.sql_query}) using: {self.connection_string}")
-        df = pl.read_sql(sql=self.sql_query, connection_uri=self.connection_string, partition_on=self.partition_on)
-        return df
+        return pl.read_database(query=self.sql_query, connection_uri=self.connection_string,
+                                partition_on=self.partition_on, engine=self.engine)
