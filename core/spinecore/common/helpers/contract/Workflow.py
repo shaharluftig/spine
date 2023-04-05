7cf68d0a2298390889ad087db3094577d376e6cd
Luftig <shaharluftig@gmail.com>
2023-04-06 01:00:01 +0300

    fix(async executor logic)

--- core/spinecore/common/helpers/contract/Workflow.py
+++ core/spinecore/common/helpers/contract/Workflow.py
@@ -1,36 +1,34 @@
 from abc import ABC
 from typing import List
 
-import networkx as nx
-
 from spinecore.common.helpers.contract.IStep import IStep
-from spinecore.common.helpers.graphs import topological_pos, default_dag_style
 
 
 class Workflow(ABC):
+    """Abstract class that represents general spine workflow"""
+
     def __init__(self, name: str, graph):
         self.name = name
         self.graph = graph
 
     def add_after(self, next_steps: List[IStep], prev_step: List[IStep]):
+        """Add step after another"""
         raise NotImplementedError()
 
     def add_last(self, next_step: IStep):
+        """Push step to the end of the workflow"""
         raise NotImplementedError()
 
-    def flat_graph(self):
+    def flat(self):
+        """Return flatten representation of the workflow"""
         raise NotImplementedError()
 
     def get_before(self, step=None):
+        """Get the previous step"""
         raise NotImplementedError()
 
-    def show_graph(self, style: dict = None) -> None:
-        try:
-            from matplotlib import pyplot as plt
-            from matplotlib import rcParams
-            rcParams.update({'figure.autolayout': True})
-            style = style if style else default_dag_style
-            nx.draw_networkx(self.graph, pos=topological_pos(self.graph), **style)
-            plt.show()
-        except ImportError:
-            raise ImportError("matplotlib must be installed in order to show graph")
+    def get_all_sub_workflows(self) -> List:
+        raise NotImplementedError
+
+    def show_workflow(self, style: dict = None) -> None:
+        raise NotImplementedError
---
+++ core/spinecore/tests/unit/executors/test_spine_executor.py
@@ -0,0 +1,39 @@
+import asyncio
+
+import polars as pl
+import pytest
+
+from spinecore.common.context.polars_context import SpinePolarsContext
+from spinecore.common.helpers.contract.IStep import IStep
+from spinecore.executors import execute
+from spinecore.workflows import DagWorkflow
+
+
+class BlockingStep(IStep):
+    async def process(self, ctx: SpinePolarsContext, df=None):
+        df = pl.DataFrame({"foo": [1, 2, 3], "bar": [None, "ham", "spam"]})
+        await asyncio.sleep(1)  # Blocking
+        return df
+
+
+class NothingStep(IStep):
+    async def process(self, ctx: SpinePolarsContext, df):
+        return df
+
+
+@pytest.mark.asyncio
+async def test_blocking_step():
+    """Tests IO Blocking step execution"""
+    # Prepare
+    ctx = SpinePolarsContext.get_context(lazy=True, config={"set_tbl_rows": 20})
+    workflow = DagWorkflow("test_blocking")
+    expected_df = pl.DataFrame({"foo": [1, 2, 3], "bar": [None, "ham", "spam"]})
+    nothing_step = NothingStep()
+    workflow.add_after([nothing_step], [BlockingStep()])
+
+    # Action
+    result = await execute(ctx, workflow)
+    df = result[0][nothing_step]
+
+    # Assert
+    assert df.frame_equal(expected_df)
